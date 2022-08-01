9b551b44c4b656a3ef61a4ea4c18d9ed8fe3b634
Alexander Pelta <abp06c@Alexanders-MacBook-Air.local>
2022-08-01 15:05:54 -0400

    updated

---
+++ students.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+const students = ["damian","alex"]
+
+app.get("/", (req, res) => {
+  let output = "<html><body><ul>";//starts
+  
+  //output = output + "hello there html" //middle
+  for(let i = 0; i < students.length; i++) {
+      output = output + 
+      '<li><input type="checkbox">' + 
+      students[i] + '</li>'
+
+  }
+
+  output = output + "</ul></body></html>"; //end
+
+  res.send(output);
+});
+
+app.listen(port, () => {
+  console.log("I was needed");
+});
